7c24d2b3bbb0d90d5cda830bebdc6ea84210b3de
이 민 서 (Lee min seo) <lmsjtsljh@naver.com>
2025-03-20 10:56:06 +0900

    Add files via upload

--- java/D8.java
+++ java/D8.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class D8 {
-    public static void main(String[] ages){
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("현관문 상태를 입력하세요 (열림/닫힘): ");
+
+
+        String doorStatus = scanner.nextLine().trim();
+
+
+        if (doorStatus.equalsIgnoreCase("열림")) {
+            System.out.println("현관문이 열렸습니다. 신호를 보냅니다!");
+
+            sendSignal();
+        } else if (doorStatus.equalsIgnoreCase("닫힘")) {
+            System.out.println("현관문이 닫혔습니다.");
+        } else {
+            System.out.println("잘못된 입력입니다.");
+        }
+
+        scanner.close();
+    }
+
+
+    public static void sendSignal() {
+
+        System.out.println("알림: 현관문이 열렸습니다!");
+
 
     }
 }
--- java/D9.java
+++ java/D9.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class D9 {
+    public static void main(String[] ages){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("내가  공부를 잘하고 있는걸까? (맞아/아니야)");
+        //answer 사용자가 입력한 값을 변수로 지정한다
+        String answer = sc.nextLine().trim();
+
+        if(answer.equals("맞아")){
+            System.out.println("그러치 그걸 너의 걸로 만들어");
+
+        } else if(answer.equals("아니야")){
+            System.out.println("자신감을 챙겨 친구");
+        } else{
+            System.out.println("뭐라는거야 제대로 적어");
+        }
+        sc.close();
+    }
 }
