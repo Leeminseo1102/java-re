8214da58a3c5fe7c5950e2bd08aab4db61ba87d2
이 민 서 (Lee min seo) <lmsjtsljh@naver.com>
2025-04-18 08:57:02 +0900

    Add files via upload

---
+++ java/D40.java
@@ -0,0 +1,12 @@
+public class D40 {
+    public static void main(String[] args) {
+
+        for(int i = 2; i <= 9; i++){
+            for(int j = 1; j <= 9; j++){
+                int result = i*j;
+                System.out.print(i + "X" + j + "=" + result + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ java/D41.java
@@ -0,0 +1,9 @@
+public class D41 {
+    public static void main(String[] args){
+
+        while(true){
+            System.out.println("Hello World");
+            break;
+        }
+    }
+}
